16a3585c3cd9a8b5dc5f6c9a8c3ea761d8527802
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-08-18 21:51:36 +0000

    add eventlistners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// wait for dom to finish loaging before running the game
+// get the button elements and add event listners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+
+})
+
 function runGame() {
 
 }
@@ -27,5 +46,6 @@ function displaySubtractquestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
+
